7988f36c905a822c1fb4e93b32800c2aea41b30d
lucas <lucas@DESKTOP-5TLC2TC>
2023-04-08 10:46:09 -0300

    Agregar archivos de proyecto.

---
+++ Clase 3 - Actividad 1/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace actividad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool continuar = true;
+
+            while (continuar) 
+            { 
+
+                System.Console.WriteLine("Digite el primer numero");
+                int n1 = int.Parse(Console.ReadLine());
+                System.Console.WriteLine("Digite el segundo numero");
+                int n2 = int.Parse(Console.ReadLine());
+                System.Console.WriteLine("Digite el tercer numero");
+                int n3 = int.Parse(Console.ReadLine());
+
+
+                    if (n1 > n2 && n1 > n3)
+                    {
+                    Console.WriteLine(n1 + " es el numero Mayor ");
+                    }
+                    else if (n2 > n1 && n2 > n3) 
+                    {
+                    Console.WriteLine(n2 + " es el numero Mayor ");
+                    }
+                    else
+                    {
+                    Console.WriteLine(n3 + " es el numero Mayor ");
+                    }
+
+
+                    //NUEVO INGRESO
+
+                bool respuestaValida = false;
+
+                while (!respuestaValida)
+                {
+                    Console.WriteLine("¿Desea calcular el Mayor de otros 3 numeros?");
+                    string respuesta = Console.ReadLine();
+
+                    //Excepcion si se ingresa respuesta != si o no
+                    try
+                    {
+                        if (respuesta.ToLower() == "si") //toLower = convertimos todo a MINUSCULA y 
+                        {                                //facilitamos el manejo de la variable
+                        respuestaValida = true;
+                        }
+                        else if (respuesta.ToLower() == "no")
+                        {
+                        respuestaValida = true;
+                        continuar = false;
+                        }
+                        else
+                        {
+                        throw new Exception("La respuesta debe ser 'si' o 'no'. Por favor, ingrese una respuesta válida.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                    Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+    }
+}
